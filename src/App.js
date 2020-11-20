cf819bba0f34268c4db5b258f2cdff475d9b3ed2
Andrei Voinea <andreivdev@protonmail.com>
2020-11-20 21:51:55 +0200

    utx and block subscribe to blockchain.info. When block is received, request details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,49 @@
 import "./App.css";
 
 function App() {
+  if ("WebSocket" in window) {
+    var ws = new WebSocket("wss://ws.blockchain.info/inv");
+
+    ws.onopen = function () {
+      ws.send(JSON.stringify({ op: "unconfirmed_sub" }));
+      ws.send(JSON.stringify({ op: "blocks_sub" }));
+    };
+
+    ws.onmessage = function (evt) {
+      const axios = require("axios");
+      let received_msg = JSON.parse(evt.data);
+      if (received_msg.op === "utx") {
+        console.log("utx " + received_msg.x.hash);
+      }
+      if (received_msg.op === "block") {
+        console.log("block " + received_msg.x.hash);
+        axios
+          .get(
+            "https://blockchain.info/rawblock/" +
+              received_msg.x.hash +
+              "?&cors=true"
+          )
+          .then(function (response) {
+            // handle success
+            console.log(response);
+          })
+          .catch(function (error) {
+            // handle error
+            console.log(error);
+          })
+          .then(function () {
+            // always executed
+          });
+      }
+    };
+
+    ws.onclose = function () {
+      alert("Connection is closed...");
+    };
+  } else {
+    alert("WebSocket NOT supported by your Browser!");
+  }
+
   return <div className="App"></div>;
 }
 
